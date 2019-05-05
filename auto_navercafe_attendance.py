d3ec57e31af544fa8b210f9a1fd1a0d9489cf299
AhnJG <arki13@naver.com>
2019-05-05 12:01:20 +0900

    Update auto_navercafe_attendance.py

--- auto_navercafe_attendance.py
+++ auto_navercafe_attendance.py
@@ -2,13 +2,6 @@ from selenium import webdriver
 from time import sleep, time
 from datetime import datetime
 
-#0.크롬드라이버를 다운받는다.
-#0.크롬 기본설정 파일의 위치를 찾는다 (%localappdata%/Google/Chrome/User Data
-#1.모든 크롬창을 닫고 실행해야한다.
-#2.클릭할 버튼들이 꼭 자신의 화면안에 나타나 있어야한다.
-#selenium을 통해서는 기존 창을 이용할 수 없고 항상 새로운 창이 뜨게된다.
-#따라서 새로운 창을 띄울 때 기존 크롬에서 사용하는 사용차 설정들을 불러온다.
-#로그인 했을 때 자동로그인 방지 문구가 뜨지 않도록 크롬 기본 설정을 로드하고 중간중간에 대기시간을 넣는다.
 options = webdriver.ChromeOptions()
 options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36')
 options.add_argument('disable-infobars')
